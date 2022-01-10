46ced7b0d423fcaedd7c3be896dc716a37f8d96a
adrianpaliz <adrianpaliz@gmail.com>
2022-01-10 11:51:52 -0500

    Changed names of variable and integers.

--- exercise_33_and_study_drills.py
+++ exercise_33_and_study_drills.py
@@ -1,16 +1,16 @@
-i = 0
-numbers = []
+numbers_list = [] # Defined an empty list called numbers_list.
+index = 0 # The index is defined to 0.
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+while index < 6: # The while loop repeatedly executes unlit the index be less than six. 
+    print "At the top index is %d" % index # Frist indented line, string with formatting syntax for print each integer of index.
+    numbers_list.append(index) # Use append method to add to numbers_list each integers from index.
 
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+    index = index + 1 # Increment index in one.
+    print "Numbers now: ", numbers_list # Print plaint text and numbers_list until be less than six.
+    print "At the bottom index is %d" % index # String for print the increment of index with formatiting syntax.
 
 
-print "The numbers: "
+print "The numbers: " # Print plain text
 
-for num in numbers:
-    print num
+for numbers in numbers_list: # For loop for iterating each integer of number_list.
+     print numbers # Print each one of te integers in numbers_list.
